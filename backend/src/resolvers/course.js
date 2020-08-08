7969e828e7bd12e8489431721eb4404f48a1c322
ZHENK <chengzhenyang@gmail.com>
2020-08-09 08:17:35 +1200

    backend fix

--- backend/src/resolvers/course.js
+++ backend/src/resolvers/course.js
@@ -1,22 +1,21 @@
 import Course from '../models/Course'
 import UserCourse from '../models/user_course'
 
-const shuffle=(arr) =>{
-        // shuffle arr
-        let arr1 = arr.slice()
-        let ctr = arr1.length;
-        let temp;
-        let index;
-        while (ctr > 0) {
-            index = Math.floor(Math.random() * ctr);
-            ctr--;
-            temp = arr1[ctr];
-            arr1[ctr] = arr1[index];
-            arr1[index] = temp;
-        }
-        //console.log(arr1)
-        return arr1
-} 
+const shuffle = (arr) => {
+    // shuffle arr
+    let arr1 = arr.slice()
+    let ctr = arr1.length;
+    let temp;
+    let index;
+    while (ctr > 0) {
+        index = Math.floor(Math.random() * ctr);
+        ctr--;
+        temp = arr1[ctr];
+        arr1[ctr] = arr1[index];
+        arr1[index] = temp;
+    }
+    return arr1
+}
 
 const CourseResolver = {
 
@@ -24,7 +23,7 @@ const CourseResolver = {
         users: async ({ id }) => {
             const course = await UserCourse.find({ course: id }).populate('user')
             if (course != null) {
-                return course.map(({user}) => {
+                return course.map(({ user }) => {
                     return {
                         id: user._id,
                         ...user._doc
@@ -56,15 +55,13 @@ const CourseResolver = {
         faces: async (root, args, context) => {
             const { courseID, count } = args
             const courses = await UserCourse.find({ course: courseID }).limit(count).populate('user')
-            console.log(courses)
             if (courses != null) {
-                const array = shuffle(courses.map(({user}) => {
+                const array = shuffle(courses.map(({ user }) => {
                     return {
                         id: user._id,
                         ...user._doc
                     }
                 }))
-                console.log(array)
                 return array
             }
             return []
@@ -89,7 +86,7 @@ const CourseResolver = {
             await Course.findByIdAndUpdate(args.id, {
                 name,
                 description,
-            }, (err, result)=> {
+            }, (err, result) => {
                 if (err) {
                     console.log(err)
                 }
