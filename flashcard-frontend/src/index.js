2e077e24fb0009a73fe8b760852b39dd50bf722e
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-08-08 20:23:54 +1200

    update to login page

--- flashcard-frontend/src/index.js
+++ flashcard-frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './pages/login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
