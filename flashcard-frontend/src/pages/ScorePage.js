22466e975d218d81f2fda1b439b89d613fd2f301
fbab920 <fbab920@aucklanduni.ac.nz>
2020-08-08 20:57:20 +1200

    Add images

--- flashcard-frontend/src/pages/ScorePage.js
+++ flashcard-frontend/src/pages/ScorePage.js
@@ -6,24 +6,33 @@ import { spacing } from '@material-ui/system';
 
 function ScorePage({score}){
     let textToShow = null;
+    let imgToShow = null;
     if(score >= 4 ) {
         textToShow = "WOW, You are amazing!!!";
+        if(score >= 5){
+            imgToShow = "./pngfuel.com (2).png";
+        }else{
+            imgToShow = "./pngfuel.com (3).png";
+        }
     }else if(score == 3){
         textToShow = "Your friends are waiting for you to remember their names!";
+        imgToShow = "./pngfuel.com (4).png";
     }else{
         textToShow = "How dare you forgot my name!";
+        imgToShow = "cry.png";
     }
 
     return(
-
         <center>
             <br />
-            <div style={{display: "flex", height: "100vh", flexDirection: "column"}}>
+            <div style={{display: "flex", height: "90vh", flexDirection: "column"}}>
                 <div style={{flex: "auto"}}>
-                    <Typography variant="h2" >{ textToShow }</Typography>
+                    <Typography variant="h3" >{ textToShow }</Typography>
                     <br />
-                    <Typography variant="h4">Score: {score}/5</Typography>
+                    <Typography variant="h4" style={{color: "grey"}}>Score: {score}/5</Typography>
                     <br /><br />
+
+                    <img src={imgToShow} width="200"></img>
                 </div>
                 <div style={{flex: 1}}>
                     <Button variant="contained" size="large" color="primary" href="/" style={{margin: "20px"}}>Back</Button>
