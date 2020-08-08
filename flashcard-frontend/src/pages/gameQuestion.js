1dab7043843337c48cc9906f2ee5c236e6a108af
Neville <nevilleloh0115@gmail.com>
2020-08-08 19:05:34 +1200

    refactor and added onclick functtion

--- flashcard-frontend/src/pages/gameQuestion.js
+++ flashcard-frontend/src/pages/gameQuestion.js
@@ -39,12 +39,29 @@ const BorderLinearProgress = withStyles((theme) => ({
   },
 }))(LinearProgress);
 
+
+
+
 function GameQuestion(props){
     const person1 = "https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
     const person2 = "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
     const person3 = "https://images.unsplash.com/photo-1521117660421-ce701ed42966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
     const person4 = "https://images.unsplash.com/photo-1521038199265-bc482db0f923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
     const classes = useStyles();
+    const handleClickf = () => {
+      alert("works")
+    }
+
+    const cardChoices = props.choices.map((choice) =>
+    <Grid item xs={6}>
+      <Paper className={classes.paper} onClick={handleClickf} >
+        
+        <center>
+        <Avatar alt="Remy Sharp" src= {choice} className={classes.large} />
+        </center>
+      </Paper>
+    </Grid>)
+    
     return(
     <div>
         <h1></h1>
@@ -55,34 +72,9 @@ function GameQuestion(props){
         <Grid item xs={12}>
           <Paper className={classes.paper}>{props.name}</Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            <center>
-            <Avatar alt="Remy Sharp" src= {props.choices[0]} className={classes.large} />
-            </center>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-          <center>
-            <Avatar alt="Remy Sharp" src= {props.choices[1]} className={classes.large} />
-            </center>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-          <center>
-            <Avatar alt="Remy Sharp" src= {props.choices[3]} className={classes.large} />
-            </center>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-          <center>
-            <Avatar alt="Remy Sharp" src= {props.choices[4]} className={classes.large} />
-            </center>
-          </Paper>
-        </Grid>
+
+        {cardChoices}
+
       </Grid>
         
     </div>)
