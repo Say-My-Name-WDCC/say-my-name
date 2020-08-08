610b3a362ae32c202018c3304c3cadd0ae718e83
fbab920 <fbab920@aucklanduni.ac.nz>
2020-08-08 19:49:37 +1200

    Add buttons using Materual UI

--- flashcard-frontend/src/pages/ScorePage.js
+++ flashcard-frontend/src/pages/ScorePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { Container } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { spacing } from '@material-ui/system';
 
 function ScorePage({score}){
     let textToShow = null;
@@ -15,15 +17,19 @@ function ScorePage({score}){
     return(
 
         <center>
-            <div className="ScorePage" >
-                
-                <h1>{ textToShow }</h1>
-                <br />
-                <p>score: {score}/5</p>
-                <br /><br />
-
-                <Button variant="contained" color="primary">Back</Button>
-                <Button variant="contained" color="primary">Play Again</Button>
+            <br />
+            <div style={{display: "flex", height: "100vh", flexDirection: "column"}}>
+                <div style={{flex: "auto"}}>
+                    <Typography variant="h2" >{ textToShow }</Typography>
+                    <br />
+                    <Typography variant="h4">Score: {score}/5</Typography>
+                    <br /><br />
+                </div>
+                <div style={{flex: 1}}>
+                    <Button variant="contained" size="large" color="primary" href="/" style={{margin: "20px"}}>Back</Button>
+                    
+                    <Button variant="contained" size="large" color="primary" href="/game">Play Again</Button>
+                </div>
             </div>
         </center>
     );
