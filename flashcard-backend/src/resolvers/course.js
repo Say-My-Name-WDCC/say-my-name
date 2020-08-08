6f821d8909c5b6b36fb8332685450de13c8eea85
Neville <nevilleloh0115@gmail.com>
2020-08-08 21:11:54 +1200

    updated query

--- flashcard-backend/src/resolvers/course.js
+++ flashcard-backend/src/resolvers/course.js
@@ -1,17 +1,32 @@
 import Course from '../models/Course'
 import UserCourse from '../models/user_course'
 
+const shuffle=(arr1) =>{
+        // shuffle arr
+        let ctr = c.length;
+        let temp;
+        let index;
+        while (ctr > 0) {
+            index = Math.floor(Math.random() * ctr);
+            ctr--;
+            temp = arra1[ctr];
+            arra1[ctr] = arra1[index];
+            arra1[index] = temp;
+        }
+        return arr1
+} 
+
 const CourseResolver = {
 
     Course: {
         users: async ({ _id }) => {
-            const course = await UserCourse.find({course: _id}).populate('user')
-            if (course!= null){
+            const course = await UserCourse.find({ course: _id }).populate('user')
+            if (course != null) {
                 return course.map(course => {
                     return course.user
                 })
             }
-            return[]
+            return []
         }
     },
 
@@ -22,6 +37,17 @@ const CourseResolver = {
         },
         courses: async (root, args, context) => {
             return await Course.find({})
+        },
+        faces: async (root, args, context) => {
+            const { courseID, count } = args
+
+            const courses = await UserCourse.find({ course: _id }).limit(count).populate('user')
+            if (courses != null) {
+                return shuffle(courses.map(course => {
+                    return course.user
+                }))
+            }
+
         }
     },
     Mutation: {
