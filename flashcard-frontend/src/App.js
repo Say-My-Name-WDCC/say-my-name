0386e12a06ccac14c55b6457f9f2adcdfe6abf0e
Hiruna Jayamanne <hiru@hiru.dev>
2020-08-08 18:11:01 +1200

    Mount GamePage to /game

--- flashcard-frontend/src/App.js
+++ flashcard-frontend/src/App.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import GameQuestion from './pages/gameQuestion';
 import './App.css';
 
 function App() {
   return (
     <Container className="App" maxWidth="sm">
       <BrowserRouter>
-        <Route path="/">
-          <HomePage name="Hiruna Jayamanne" courses={["SOFTENG211"]} />
-        </Route>
+        <Switch>
+          <Route path="/game">
+            <GameQuestion 
+              name={"A very cool names"} 
+              choices={[
+                "https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+                "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
+                "https://images.unsplash.com/photo-1521117660421-ce701ed42966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+                "https://images.unsplash.com/photo-1521038199265-bc482db0f923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+              ]}
+              score={50} 
+              />
+          </Route>
+          <Route path="/">
+            <HomePage name="Hiruna Jayamanne" courses={["SOFTENG211"]} />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </Container>
   );
--- flashcard-frontend/src/index.js
+++ flashcard-frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import GameQuestion from './pages/gameQuestion';
 
 
 
@@ -16,16 +15,7 @@ const a = "something"
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameQuestion 
-    name={"A very cool names"} 
-    choices={[
-      "https://images.unsplash.com/photo-1527082395-e939b847da0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-      "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
-      "https://images.unsplash.com/photo-1521117660421-ce701ed42966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      "https://images.unsplash.com/photo-1521038199265-bc482db0f923?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-    ]}
-    score={50} 
-    />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
